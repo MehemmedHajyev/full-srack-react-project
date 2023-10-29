d1cfe5eb53a3d0e2f0305f518211e6ead0f4e103
MehemmedHajyev <hajyeffmehemmed8@gmail.com>
2023-10-29 17:17:23 +0400

    costume slider add

--- front-end/src/component/Slider/Slider.jsx
+++ front-end/src/component/Slider/Slider.jsx
@@ -3,8 +3,24 @@ import SliderItem from "./SliderItem"
 import './slider.css'
 
 const Slider = () => {
-  const [currentSlide, setCurrentSlide] = useState(0)
+  const [currentSlide, setCurrentSlide] = useState(1)
 
+
+
+
+
+  const nextSlide = () => {
+    setCurrentSlide((nextSlide) => (nextSlide + 1) % 3)
+    // next Slide buttnon yapimi 
+
+  }
+
+
+  const prevSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide - 1 + 3) % 3)
+    // prev Slide buttnon yapimi 
+
+  }
   return (
     <section className="slider">
       <div className="slider-elements">
@@ -18,20 +34,28 @@ const Slider = () => {
 
 
         <div className="slider-buttons">
-          <button >
+          <button onClick={prevSlide} >
             <i className="bi bi-chevron-left"></i>
           </button>
-          <button >
+          <button onClick={nextSlide}  >
             <i className="bi bi-chevron-right"></i>
           </button>
         </div>
         <div className="slider-dots">
-          <button className={`slider-dot ${currentSlide === 0 ? 'active' : ''}  `} >
+          <button className={`slider-dot ${currentSlide === 0 ? 'active' : ''}  `}
+            onClick={() => setCurrentSlide(0)}
+          >
             <span></span>
           </button>
-          <button className={`slider-dot ${currentSlide === 1 ? 'active' : ''}  `} >
+          <button className={`slider-dot ${currentSlide === 1 ? 'active' : ''}  `}
+            onClick={() => setCurrentSlide(1)}
+
+          >
             <span></span>
-          </button> <button className={`slider-dot ${currentSlide === 2 ? 'active' : ''}  `} >
+          </button> <button className={`slider-dot ${currentSlide === 2 ? 'active' : ''}  `}
+            onClick={() => setCurrentSlide(2)}
+
+          >
             <span></span>
           </button>
         </div>
